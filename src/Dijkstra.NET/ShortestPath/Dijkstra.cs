924bb5a09c53f5fdf43c190847052ff62a612d50
Lorenzo Gardenghi <lorenzo.gardenghi@lab51.it>
2023-05-25 15:56:58 +0200

    Get dictionary of the distances from destination node

--- src/Dijkstra.NET/ShortestPath/Dijkstra.cs
+++ src/Dijkstra.NET/ShortestPath/Dijkstra.cs
@@ -6,6 +6,60 @@ namespace Dijkstra.NET.ShortestPath
 {
     internal static class Dijkstra
     {
+        public static Dictionary<uint, Dictionary<string, object?>> GetDistancesFromDestination(IDijkstraGraph graph, uint to, int depth)
+        {
+            var distance = new Dictionary<uint, Dictionary<string, object?>>();
+            List<uint> q = new List<uint>();
+            var d = new Dictionary<uint, int> { [to] = 0 };
+            var current = new HashSet<uint>();
+
+
+            Dictionary<string, object?> innerDict = new Dictionary<string, object?>();
+            innerDict.Add("parent", null);
+            innerDict.Add("distance", 0);
+
+            distance.Add(to, innerDict);
+
+            q.Add(to);
+
+            int Distance(uint key)
+            {
+                return distance.ContainsKey(key) ? (int)distance[key]["distance"] : Int32.MaxValue;
+            }
+
+            do
+            {
+                uint u = q[0];
+                q.RemoveAt(0);
+
+                if (depth == d[u])
+                {
+                    continue;
+                }
+
+                graph[u]((node, cost) =>
+                {
+                    if (Distance(node) > Distance(u) + cost)
+                    {
+                        if (current.Contains(node))
+                        {
+                            q.Remove(node);
+                        }
+
+                        var distanceDict = new Dictionary<string, object> { ["parent"] = u, ["distance"] = Distance(u) + cost };
+                        //distance.Add(node, distanceDict);
+                        distance[node] = distanceDict;
+
+                        q.Add(node);
+                        d[node] = d[u] + 1;
+                    }
+                });
+            } while (q.Count > 0);
+
+
+            return distance;
+        }
+
         public static ShortestPathResult GetShortestPath(IDijkstraGraph graph, uint from, uint to, int depth)
         {
             var path = new Dictionary<uint, uint>();
